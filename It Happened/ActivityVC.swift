933ffef63c27e83d945c1c63eae446179fd32fd6
Drew Lanning <drewLanning@Drews-MacBook-Air-2.local>
2017-09-01 09:18:11 -0400

    added colors to style file, added activity Cell to main view and styled and tested

--- It Happened/ActivityVC.swift
+++ It Happened/ActivityVC.swift
@@ -8,27 +8,55 @@
 
 import UIKit
 
-class ActivityVC: UIViewController {
-
+class ActivityVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+  @IBOutlet weak var headingLbl: UILabel!
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var newButton: UIButton!
+  @IBOutlet weak var helpButton: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    // Do any additional setup after loading the view, typically from a nib.
+    styleViews()
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+  
+  func styleViews() {
+    headingLbl.textColor = Colors.primary
+    self.view.backgroundColor = Colors.black
+    tableView.backgroundColor = Colors.black
+    newButton.setTitleColor(Colors.accent2, for: .normal)
+    helpButton.setTitleColor(Colors.accent1, for: .normal)
   }
   
   override func viewWillAppear(_ animated: Bool) {
     self.navigationController?.setNavigationBarHidden(true, animated: true)
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
+  
   @IBAction func newTapped(sender: UIButton) {
-    let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "InstanceVC")
-    self.navigationController?.pushViewController(nextVC!, animated: true)
+    
   }
-
+  
+  // MARK: Tableview Functions
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "activityCell")
+    return cell!
+  }
+  
 }
 
--- It Happened/Style.swift
+++ It Happened/Style.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2017 Drew Lanning. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum Colors {
+  
+  static let black = UIColor(colorLiteralRed: 10/255, green: 30/255, blue: 35/255, alpha: 1.0)
+  static let primary = UIColor(colorLiteralRed: 39/255, green: 174/255, blue: 96/255, alpha: 1.0)
+  static let secondary = UIColor(colorLiteralRed: 242/255, green: 193/255, blue: 78/255, alpha: 1.0)
+  static let accent1 = UIColor(colorLiteralRed: 95/255, green: 173/255, blue: 86/255, alpha: 1.0)
+  static let accent2 = UIColor(colorLiteralRed: 247/255, green: 129/255, blue: 84/255, alpha: 1.0)
+  static let accent3 = UIColor(colorLiteralRed: 180/255, green: 67/255, blue: 108/255, alpha: 1.0)
+  
+}
