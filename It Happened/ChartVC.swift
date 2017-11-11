778201bfc2c33573cd56ed98bd20ed909ac473a6
Drew Lanning <drewLanning@Drews-MacBook-Air-2.local>
2017-11-11 09:35:53 -0500

    dummy data showing bars in bar chart

--- It Happened/ChartVC.swift
+++ It Happened/ChartVC.swift
@@ -9,17 +9,19 @@
 import UIKit
 import JBChartView
 
-class ChartVC: UIViewController {
+class ChartVC: UIViewController, JBBarChartViewDelegate, JBBarChartViewDataSource {
   
   // MARK: Properties
   var activity: Activity?
   @IBOutlet weak var emptyDataLbl: UILabel!
-  @IBOutlet weak var chartView: JBChartView!
+  @IBOutlet weak var chartView: JBBarChartView!
   
   // MARK: Methods
   override func viewDidLoad() {
     super.viewDidLoad()
     styleViews()
+    chartView.dataSource = self
+    chartView.delegate = self
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -31,15 +33,32 @@ class ChartVC: UIViewController {
     self.view.backgroundColor = Settings().colorTheme[.background]
     self.title = activity?.name!
     emptyDataLbl.textColor = Settings().colorTheme[.accent2]
-    chartView.backgroundColor = .red
+    chartView.backgroundColor = .black
   }
   
   fileprivate func updateViews() {
     let hasActivity = self.activity!.instance!.count > 0
     emptyDataLbl.isHidden = hasActivity
     chartView.isHidden = !hasActivity
+    if !chartView.isHidden {
+      chartView.minimumValue = 0.0
+      chartView.maximumValue = 100.0
+      chartView.reloadData()
+    }
   }
   
   // MARK: Chart Methods
+  
+  func numberOfBars(in barChartView: JBBarChartView!) -> UInt {
+    return 10
+  }
+  
+  func barChartView(_ barChartView: JBBarChartView!, heightForBarViewAt index: UInt) -> CGFloat {
+    return 50
+  }
+  
+  func barChartView(_ barChartView: JBBarChartView!, colorForBarViewAt index: UInt) -> UIColor! {
+    return .red
+  }
   
 }
